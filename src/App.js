34221184cca6c7b3c1d03aca016fb78769251a9c
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2022-12-10 09:40:09 +0000

    React PWA + Chakra UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { 
+  Flex 
+} from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Flex>
+      fdsdfsd
+    </Flex>
+  );
+}
+
+export default App;
